286cad57a9151b3687cca02d1951017baf81c1fd
Matheus Pazinati <matheuspazinati@gmail.com>
2023-05-11 12:37:23 -0300

    Adiciona rota para criacao de usuario

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,31 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { z } from 'zod'
+import { knex } from "../database";
+import { randomUUID } from 'node:crypto'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', async (request: FastifyRequest, reply: FastifyReply) => {
-    console.log(request.body)
-    reply.status(201).send()
+    const UserRegisterSchema = z.object({
+      name: z.string().min(2, { message: "Seu nome deve possui no mínimo 2 letras" }),
+      email: z.string().email({ message: "E-mail inválido" }),
+      password: z.string().min(6, { message: "A senha deve ter no mínimo 6 caracteres" }),
+      confirmPassword: z.string()
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: 'As senhas devem ser iguais',
+      path: ["confirmPassword"]
+    })
+
+    const { name, email, password } = UserRegisterSchema.parse(request.body)
+
+    await knex('users').insert({
+      id: randomUUID(),
+      name,
+      email,
+      password
+    })
+
+    return reply.status(201).send()
+    
   })
 }
